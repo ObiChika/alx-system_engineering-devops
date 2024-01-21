deb5203ce56bbfd8803856e80aa1cee932370191
ObiChika <amandachika85@gmail.com>
2024-01-21 10:46:22 -0800

    Updating SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Setting up my .ssh/config files using puppet
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+}
